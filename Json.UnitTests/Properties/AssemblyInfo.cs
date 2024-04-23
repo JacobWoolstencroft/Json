892fd2a9946cd9bf0a233005ae85497c547eb762
Jacob Woolstencroft <jacob_woolie@yahoo.com>
2024-04-23 17:31:24 -0500

    Version 1.2

--- Json.UnitTests/Properties/AssemblyInfo.cs
+++ Json.UnitTests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("2ab3d1e9-579e-4dda-9831-c55fb32942ff")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]

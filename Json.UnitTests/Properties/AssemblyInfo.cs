66733584c364ad72d96460ef723fe542a0345019
Jacob Woolstencroft <jacob_woolie@yahoo.com>
2024-04-23 14:06:58 -0500

    Added copyright info to Json.UnitTests

--- Json.UnitTests/Properties/AssemblyInfo.cs
+++ Json.UnitTests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © Rita Woolstencroft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
